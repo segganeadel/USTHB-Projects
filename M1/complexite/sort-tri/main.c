7659d00708bb8ade769f006d0fc4e11661a4eac3
segganeadel <segganeadel@gmail.com>
2022-11-24 16:32:38 +0100

    Added table inputs

--- M1/complexite/sort-tri/main.c
+++ M1/complexite/sort-tri/main.c
@@ -27,31 +27,68 @@ void printArray(int arr[], int size)
     }
 }
 
-int main()
-{
-	
-	
-    long int n = 5*pow(10,4);
-    int arr[n+1];
+int *ran(int* arr, int n){
 
-    for (size_t i = 0; i < n; i++)
+    for (int i = 0; i < n; i++)
     {
         arr[i] = rand();
     }
-    
-    time_t begin = time(NULL);
+    return arr;
+}
 
-    triInsertion(arr,n);
+int *croissant(int* arr, int n){
+
+    for (int i = 0; i < n; i++)
+    {
+        arr[i] = i;
+    }
+    return arr;
+}
+
+int *decroissant(int* arr,int n){
+
+    for (int i = 0; i < n; i++)
+    {
+        arr[i] = n-i;
+    }
+    return arr;
+}
+
+int main()
+{
+    clock_t start_t, end_t, total_t;
+
+	int *arr;
+	
+    long int n = 1*pow(10,2);
+
+    arr = (int*)malloc(n*sizeof(int));
+
+    
+
+    //arr = croissant(arr,n);
+    //arr = decroissant(arr,n);
+    arr = ran(arr,n);
+
+    printArray(arr, n);
+    
+    printf("\n\n");
+
+    start_t = clock();
+
+    //triInsertion(arr,n);
     //triBulle(arr, n);
     //triRapide(arr,0,n-1);
     //triFusion(arr,0,n-1);
-    //triTas(arr, n-1);
+    triTas(arr, n);
 
-    time_t end = time(NULL);
+    end_t = clock();
+
+    total_t = (end_t - start_t) / CLOCKS_PER_SEC;
 
     printArray(arr, n);
 
-    printf("\nThe elapsed time is %ld seconds\n", (end - begin));
+    printf("\nThe elapsed time is %f seconds\n", (double)(total_t));
 
     return 0;
 }

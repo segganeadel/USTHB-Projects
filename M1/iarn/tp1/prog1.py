9026cc9cc331f9925d9f00891a333e33b04e9fca
segganeadel <segganeadel@gmail.com>
2023-02-14 17:26:36 +0100

    iarn tp1

---
+++ M1/iarn/tp1/prog1.py
@@ -0,0 +1,26 @@
+
+import numpy as np
+from sklearn.linear_model import LinearRegression
+
+#Pass the value -1 , NumPy will calculate this number for you.
+x = np.array([5, 15, 25, 35, 45, 55]).reshape((-1, 1))
+print(x)
+
+y = np.array([5, 20, 14, 32, 22, 38])
+print(y)
+
+model = LinearRegression()
+model.fit(x, y)
+
+
+
+r_sq = model.score(x, y) # return the error
+print('coefficient of determination:', r_sq)
+# The attributes of model are .intercept_, which represents the coefficient, 𝑏₀ 
+# and .coef_, which represents 𝑏₁:
+print('intercept:', model.intercept_) # b, y=ax+b
+print('slope:', model.coef_) #a
+xp = np.array([43, 53]).reshape((-1, 1))
+y_pred = model.predict(xp)
+print('predicted response:', y_pred, sep='\n')
+# x=15, y=0.54*15+5.63=13,73
